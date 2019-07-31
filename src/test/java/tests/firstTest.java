c2372f2fbe0435e3987108905d4a22e2cd4badc1
mustafakayauta <51256836+mustafakayauta@users.noreply.github.com>
2019-07-30 20:31:56 -0500

    Update firstTest.java
    I did from github

--- src/test/java/tests/firstTest.java
+++ src/test/java/tests/firstTest.java
@@ -7,5 +7,6 @@ public class firstTest {
         System.out.println("Hello mustafa");
         System.out.println("hello mustafa");
         System.out.println("i made changes");
+        System.out.println("directly from github");
     }
 }
